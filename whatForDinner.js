f6cbcab0f4013f67274844dc3dd4fe04dadc5764
gmacfire <42918794+gmacfire@users.noreply.github.com>
2018-11-15 11:18:05 -0500

    Create whatsForDinner.js
    project for intro to js

---
+++ whatForDinner.js
@@ -0,0 +1,24 @@
+//  Wooden Table
+background(186, 145, 20); 
+ellipse(200, 200, 350, 350); 
+//  Plate
+fill(255, 119, 0);
+ellipse(200, 200, 200, 200);
+ellipse(200, 200, 300, 300); 
+//  Crust
+fill(224, 163, 103);
+rect (126,91,151,22);
+//  Tomato Sauce
+fill (247, 19, 19);
+triangle(126,112,277,112,202,297);
+//  Pepperoni
+fill(145, 47, 65);
+ellipse(166,146,40,40);
+ellipse(228,163,40,40);
+ellipse(200,220,40,40);
+//  Green Pepper
+fill(10, 130, 32);
+triangle(251,151,261,131,219,120);
+triangle(193,192,169,190,198,166);
+triangle(215,253,201,277,197,248);
+triangle(238,191,210,185,226,204);
