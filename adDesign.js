038a1e1a9e207e74507dc247861d8c25a9f368d7
gmacfire <42918794+gmacfire@users.noreply.github.com>
2018-11-15 11:21:16 -0500

    Create adDesign.js
    could be looked at again

---
+++ adDesign.js
@@ -0,0 +1,8 @@
+fill(255, 0, 0);
+textSize(30);
+text("GMACFIRE IS BACK", 10, 30);
+textSize(10);
+text("Watch out boi!", 10, 87);
+rect(100,100,100,100);
+ellipse(195,172,100,100);
+ellipse(195,172,10,10);
